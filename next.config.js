fa73a13a629517602a58c0533d972d2e698b4173
Kanisk Chakraborty <57884127+chakrakan@users.noreply.github.com>
2022-04-30 21:14:57 -0400

    add image compat

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const { withContentlayer } = require("next-contentlayer");
 const nextConfig = withContentlayer({
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
 });
 
 module.exports = nextConfig;
